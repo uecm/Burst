28d84b43b6d8cddf3e267cb603baebba1213ecc9
uecm <uecmegor@gmail.com>
2017-06-28 20:11:57 +0300

    Initial Commit

---
+++ Burst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Burst
+//
+//  Created by Egor on 6/28/17.
+//  Copyright © 2017 egor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Burst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Burst
+//
+//  Created by Egor on 6/28/17.
+//  Copyright © 2017 egor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
